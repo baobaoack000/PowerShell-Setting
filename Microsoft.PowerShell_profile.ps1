c9a78daff050e8b24a50deec6768cfbf9c390a81
Nguyen Gia Bao <Baobaoack000@gmail.com>
2025-04-07 21:12:57 +0700

    Powershell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,13 @@
+# Modules
+Import-Module -Name Terminal-Icons
+Import-Module -Name oh-my-posh
+Import-Module oh-my-posh
+Set-PoshPrompt -Theme larserikfinholt
+
+
+
+
+
+# Custome Commands
+New-Alias -Name morning -Value C:\Users\Admin\browser-automate.ps1
+
